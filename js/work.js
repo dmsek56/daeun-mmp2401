35ecb7270f4ac961c58a3467dfd86534d15bf5c0
dmsek56 <daeun.kang@f706-02-34140.bmcc.cuny.edu>
2016-03-03 18:14:36 -0500

    second commit

---
+++ js/work.js
@@ -0,0 +1,18 @@
+$(document).ready(function (){
+  console.log("js loaded!");
+    
+  $('.hide-title img').mouseenter(function (){
+    $(this).siblings().animate({
+        opacity: "1",
+        right:"50px"
+    });
+  });
+    
+    $('.hide-title img').mouseenter(function (){
+    $(this).siblings().animate({
+        opacity: "0",
+        right:"0"
+    });
+});
+
+});
